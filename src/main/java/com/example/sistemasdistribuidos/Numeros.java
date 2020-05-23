71ceb230306eaa74520856e9ee06d73b8596065e
Caioww <sousinhacaio@gmail.com>
2020-05-23 18:01:23 -0300

    objeto valores + indice

---
+++ src/main/java/com/example/sistemasdistribuidos/Numeros.java
@@ -0,0 +1,34 @@
+package com.example.sistemasdistribuidos;
+
+public class Numeros {
+	
+	public String numero;
+	public int indice;
+	
+	public Numeros(String numero,int indice){
+        this.numero=numero;
+        this.indice=indice;
+    }
+	
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public int getIndice() {
+		return indice;
+	}
+
+	public void setIndice(int indice) {
+		this.indice = indice;
+	}
+
+	
+	
+
+	
+
+}
